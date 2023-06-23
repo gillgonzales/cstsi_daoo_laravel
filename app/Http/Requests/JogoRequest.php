9756463dfba791b076a1b9dc3d4aefaf30b46215
Miguel Rodrigues <miguelmoralesrodrigues371@gmail.com>
2023-06-23 10:09:34 -0300

    Fix: nome do campo(name para nome) + validação do tipo url

--- app/Http/Requests/JogoRequest.php
+++ app/Http/Requests/JogoRequest.php
@@ -14,18 +14,19 @@ class JogoRequest extends FormRequest
   public function rules()
   {
     return [
-      'name' => 'required | string | max:50',
-      'urlFoto' => 'required | string | max:200',
+      'nome' => 'required | string | max:50',
+      'urlFoto' => 'required | url | max:200',
     ];
   }
 
   public function messages() 
   {
     return [
-      'name.required' => 'Nome obtigatório.',
-      'name.max' => 'Nome não deve conter mais de 50 caracteres.',
+      'nome.required' => 'Nome obtigatório.',
+      'nome.max' => 'Nome não deve conter mais de 50 caracteres.',
       'urlFoto.required' => 'URL da imagem obrigatório.',
       'urlFoto.max' => 'URL da imagem não deve conter mais de 200 caracteres.',
+      'urlFoto.url' => 'Informe uma URL válida para a foto.',
     ];
   }
 }
